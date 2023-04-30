ddf1034db01552d30edc26c901435b72d7c53172
IlanVaks <ilanvaks@gmail.com>
2023-04-29 22:07:24 -0400

    Finished-weather-app

--- src/components/current-weather/CurrentWeather.jsx
+++ src/components/current-weather/CurrentWeather.jsx
@@ -1,15 +1,39 @@
 import "./currentWeather.css"
 
-export default function CurrentWeather() {
+export default function CurrentWeather({data}) {
 
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Belgrade</p>
-          <p className="weather-description">Sunny </p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img alt="weather" className="weather-icon" src="icons/sunAgain.png" />
+        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
+      </div>
+      <div className="bottom">
+        <p className="temperature">{Math.round(data.main.temp)}°F</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">{Math.round(data.main.feels_like)}°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">{data.wind.speed}mph</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">{data.main.pressure}</span>
+          </div>
+        </div>
       </div>
     </div>
   )
